f1efde7f9f1b7c57f52b6c542d36de0c8e6f356d
Emilio Platzer <emilioplatzer@gmail.com>
2025-08-20 18:19:52 -0300

    Base de datos inicial

---
+++ recursos/creacion-db.sql
@@ -0,0 +1,6 @@
+create user aida_owner nologin;
+create user aida_admin password 'cambiar_esta_clave';
+
+create database aida_db owner aida_owner;
+grant connect on database aida_db to aida_admin;
+
