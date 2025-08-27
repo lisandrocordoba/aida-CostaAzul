f677a93e7f66aeb8aaf6f8c953afe5bd09f6cc62
Emilio Platzer <emilioplatzer@gmail.com>
2025-08-27 18:17:22 -0300

    limpiamos la tabla alumnos

--- recursos/ejemplo-local-sets.bat
+++ recursos/ejemplo-local-sets.bat
@@ -2,4 +2,4 @@ set PGUSER=aida_admin
 set PGPASSWORD=cambiar_esta_clave
 set PGHOST=localhost
 set PGPORT=5432
-set PGDATABASE=admin_db
+set PGDATABASE=aida_db
--- src/cli.js
+++ src/cli.js
@@ -1,26 +1,35 @@
 import { Client } from 'pg'
-const client = new Client()
-await client.connect()
- 
 import { readFile } from 'node:fs/promises';
 
-const filePath = `recursos/alumnos.csv`;
-const contents = await readFile(filePath, { encoding: 'utf8' });
-
-const header = contents.split(/\r?\n/)[0];
-const columns = header.split(',').map(col => col.trim());
-
-const dataLines = contents.split(/\r?\n/).slice(1).filter(line => line.trim() !== '');
-
-for (const line of dataLines) {
-    const values = line.split(',');
-    const query = `
-        INSERT INTO aida.alumnos (${columns.join(', ')}) VALUES 
-            (${values.map((value) => value == '' ? 'null' : `'` + value + `'`).join(', ')})
-    `;
-    console.log(query)
-    const res = await client.query(query)
-    console.log(res.command, res.rowCount)
+async function leerYParsearCsv(filePath){
+    const contents = await readFile(filePath, { encoding: 'utf8' });
+    const header = contents.split(/\r?\n/)[0];
+    const columns = header.split(',').map(col => col.trim());
+    const dataLines = contents.split(/\r?\n/).slice(1).filter(line => line.trim() !== '');
+    return {dataLines, columns};
 }
 
-await client.end()
+async function refrescarTablaAlumnos(clientDb, listaDeAlumnosCompleta, columnas){
+    await clientDb.query("DELETE FROM aida.alumnos");
+    for (const line of listaDeAlumnosCompleta) {
+        const values = line.split(',');
+        const query = `
+            INSERT INTO aida.alumnos (${columnas.join(', ')}) VALUES 
+                (${values.map((value) => value == '' ? 'null' : `'` + value + `'`).join(', ')})
+        `;
+        console.log(query)
+        const res = await clientDb.query(query)
+        console.log(res.command, res.rowCount)
+    }
+}
+
+async function principal(){
+    const clientDb = new Client()
+    const filePath = `recursos/alumnos.csv`;
+    await clientDb.connect()
+    var {dataLines: listaDeAlumnosCompleta, columns: columnas} = await leerYParsearCsv(filePath)
+    await refrescarTablaAlumnos(clientDb, listaDeAlumnosCompleta, columnas);
+    await clientDb.end()
+}
+
+principal();
