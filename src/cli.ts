a28a95a5230aaf2eae52b475ed399b777b8ba7fa
Emilio Platzer <emilioplatzer@gmail.com>
2025-08-27 18:44:40 -0300

    Paso a TS

--- src/cli.ts
+++ src/cli.ts
@@ -14,7 +14,7 @@ async function refrescarTablaAlumnos(clientDb, listaDeAlumnosCompleta, columnas)
     for (const line of listaDeAlumnosCompleta) {
         const values = line.split(',');
         const query = `
-            INSERT INTO aida.alumnos (${columnas.join(', ')}) VALUES 
+            INSERT INTO aida.alumnos (${columnas.join(', ')}) VALUES
                 (${values.map((value) => value == '' ? 'null' : `'` + value + `'`).join(', ')})
         `;
         console.log(query)
@@ -23,12 +23,37 @@ async function refrescarTablaAlumnos(clientDb, listaDeAlumnosCompleta, columnas)
     }
 }
 
+async function obtenerPrimerAlumnoQueNecesitaCertificado(clientDb){
+    const sql = `SELECT *
+    FROM aida.alumnos
+    WHERE titulo IS NOT NULL AND titulo_en_tramite IS NOT NULL
+    ORDER BY egreso
+	LIMIT 1`;
+    const res = await clientDb.query(sql)
+    if (res.rows.length > 0){
+        return res.rows[0];
+    } else {
+        return null;
+    }
+}
+
+
+async function generarCertificadoParaAlumno(clientDb, alumno){
+    console.log('alumno', alumno);
+}
+
 async function principal(){
     const clientDb = new Client()
     const filePath = `recursos/alumnos.csv`;
     await clientDb.connect()
     var {dataLines: listaDeAlumnosCompleta, columns: columnas} = await leerYParsearCsv(filePath)
     await refrescarTablaAlumnos(clientDb, listaDeAlumnosCompleta, columnas);
+    var alumno = await obtenerPrimerAlumnoQueNecesitaCertificado(clientDb);
+    if (alumno == null){
+        console.log('No hay alumnos que necesiten certificado');
+    } else {
+        await generarCertificadoParaAlumno(clientDb, alumno);
+    }
     await clientDb.end()
 }
 
