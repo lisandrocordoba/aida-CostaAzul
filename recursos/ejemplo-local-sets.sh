990f1081845bb89698fd22c3dbeb75a54e10ed1a
Lisandro Cordoba <lisandrocordoba11@gmail.com>
2025-08-30 16:20:19 -0300

    agrego el .sh para variables de ambiente en linux, pero no puedo levantar la db

---
+++ recursos/ejemplo-local-sets.sh
@@ -0,0 +1,17 @@
+## Lo mismo que el .bat, pero para LINUX
+
+export PGUSER=aida_admin
+export PGPASSWORD=cambiar_esta_clave
+export PGHOST=localhost
+export PGPORT=5432
+export PGDATABASE=aida_db
+
+echo "Variables de entorno de PostgreSQL configuradas:"
+echo "Usuario: $PGUSER"
+echo "Host: $PGHOST"
+echo "Puerto: $PGPORT"
+echo "Base de datos: $PGDATABASE"
+
+# Ejecutar la aplicación con las variables configuradas
+echo "Ejecutando la aplicación..."
+node ./src/cli.ts
