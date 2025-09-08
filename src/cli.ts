55a66874379b50ef0dc08373d489d586e53a870f
Lisandro <lisandrocordoba11@gmail.com>
2025-09-07 21:06:41 -0300

    agregamos las funcionalidades: 1) generar certificados segund fecha pasada por parametro 2) generar certificados segun LU pasado por parametro

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,23 @@
 import { Client } from 'pg'
 import { readFile, writeFile } from 'node:fs/promises';
 
+
+function isValidDate(dateStr) {
+    // chequeo de formato primero
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(dateStr)) return false;
+
+    const [year, month, day] = dateStr.split('-').map(Number);
+
+    // Mes en JS es 0–11
+    const d = new Date(year, month - 1, day);
+
+    return (
+        d.getFullYear() === year &&
+        d.getMonth() === month - 1 &&
+        d.getDate() === day
+    );
+}
+
 async function leerYParsearCsv(filePath){
     const contents = await readFile(filePath, { encoding: 'utf8' });
     const header = contents.split(/\r?\n/)[0];
@@ -28,24 +45,43 @@ async function refrescarTablaAlumnos(clientDb, filepath){
     }
 }
 
-async function obtenerPrimerAlumnoQueNecesitaCertificado(clientDb){
-    const sql = `SELECT *
-    FROM aida.alumnos
-    WHERE titulo IS NOT NULL AND titulo_en_tramite IS NOT NULL
-    ORDER BY egreso
-	LIMIT 1`;
-    const res = await clientDb.query(sql)
-    if (res.rows.length > 0){
-        return res.rows[0];
-    } else {
-        return null;
-    }
-}
 async function generarCertificadosSegunFechaEnTramite(clientDb, fecha){
-    // buscar todos los alumnos que tengan titulo_en_tramite = parametro
-    // for alumno in alumnos_con_fecha{
-    //   generarCertificadoParaAlumno(alumno)
-    // }
+    if (!isValidDate(fecha)){
+        console.error('La fecha debe tener el formato YYYY-MM-DD');
+        process.exit(1);
+    }
+
+    // Buscamos todos los alumnos que tengan titulo_en_tramite = parametro
+    const sql = `
+    SELECT *
+    FROM aida.alumnos
+    WHERE titulo IS NOT NULL
+          AND titulo_en_tramite = $1`;
+
+    const alumnos_con_fecha = await clientDb.query(sql, [fecha]);
+
+    // Generamos los certificados para cada uno
+    for(const alumno of alumnos_con_fecha.rows){
+        await generarCertificadoParaAlumno(`recursos/plantilla-certificado.html`, alumno);
+    }
+    console.log(`Se encontraron ${alumnos_con_fecha.rows.length} alumnos con titulo_en_tramite = ${fecha}`);
+}
+
+async function generarCertificadosSegunLU(clientDb, lu){
+    // Buscamos todos los alumnos que tengan LU = parametro
+    const sql = `
+    SELECT *
+    FROM aida.alumnos
+    WHERE titulo IS NOT NULL
+          AND titulo_en_tramite IS NOT NULL
+          AND lu = $1`;
+    const alumnos_con_lu = await clientDb.query(sql, [lu]);
+
+    // Generamos los certificados para cada uno
+    for(const alumno of alumnos_con_lu.rows){
+        await generarCertificadoParaAlumno(`recursos/plantilla-certificado.html`, alumno);
+    }
+    console.log(`Se encontraron ${alumnos_con_lu.rows.length} alumnos con LU = ${lu}`);
 }
 
 function pasarAStringODarErrorComoCorresponda(value){
@@ -59,7 +95,6 @@ function pasarAStringODarErrorComoCorresponda(value){
     return result;
 }
 
-
 async function generarCertificadoParaAlumno(pathPlantilla, alumno){
     let certificado = await readFile(pathPlantilla, { encoding: 'utf8' });
     for (const [key, value] of Object.entries(alumno)) {
@@ -68,36 +103,31 @@ async function generarCertificadoParaAlumno(pathPlantilla, alumno){
             pasarAStringODarErrorComoCorresponda(value)
         );
     }
-    await writeFile(`recursos/certificado-para-imprimir.html`, certificado, 'utf-8');
-    console.log('certificado impreso para alumno', alumno.lu);
+    await writeFile(`certificados/certificado-${alumno.nombres}.html`, certificado, 'utf-8');
+    console.log('certificado impreso para alumno', alumno.nombres);
 }
 
 async function principal(){
-    const clientDb = new Client()
-    await clientDb.connect()
+    const clientDb = new Client();
+    await clientDb.connect();
 
     const comando = process.argv[process.argv.length-2];
     const parametro = process.argv[process.argv.length-1];
 
     if (comando == '--archivo'){
         await refrescarTablaAlumnos(clientDb, parametro);
-        process.exit(1);
     } else if (comando == '--fecha'){
         await generarCertificadosSegunFechaEnTramite(clientDb, parametro);
-        process.exit(1);
-    } else {
-        console.log('Uso: node cli.js --archivo <ruta-al-archivo-csv>')
-        process.exit(1);
+    } else if (comando == '--lu'){
+        await generarCertificadosSegunLU(clientDb, parametro);
+    }
+    else {
+        console.error('Comando no reconocido. Usar --archivo <path> o --fecha <YYYY-MM-DD> o --lu <LU>');
     }
 
-    var alumno = await obtenerPrimerAlumnoQueNecesitaCertificado(clientDb);
-    if (alumno == null){
-        console.log('No hay alumnos que necesiten certificado');
-    } else {
-        await generarCertificadoParaAlumno(`recursos/plantilla-certificado.html`, alumno);
-    }
-
-    await clientDb.end()
+    await clientDb.end();
+    console.log('terminado');
+    process.exit(0);
 }
 
 principal();
