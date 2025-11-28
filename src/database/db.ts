45a3c115499368a2fa4220fd1f322f43530c1131
Lisandro <lisandrocordoba11@gmail.com>
2025-11-28 01:21:22 -0300

    arreglamos error de conexion a la base: elegir si estamos en prod o dev

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,3 +1,15 @@
 import { Pool } from 'pg';
 
-export const pool = new Pool();
+// Cliente DB para el modulo
+let pool: Pool;
+if (process.env.IS_DEVELOPMENT === 'true') {
+    pool = new Pool();
+} else {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL
+    });
+}
+pool.connect();
+
+
+
