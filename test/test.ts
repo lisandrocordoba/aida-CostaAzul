b93a70682f1c758f27819111be76ceef8398dfaf
Lisandro <lisandrocordoba11@gmail.com>
2026-01-22 23:48:46 -0300

    test: agregar casos de error de login
    en total quedan testeados:
    - logueo valido
    - logueo invalido por credenciales invalidad
    - logueo invalido por falta de parametros

--- test/test.ts
+++ test/test.ts
@@ -26,4 +26,35 @@ describe('Auth Endpoints', function() {
             message: "Autenticación exitosa"
         }, "El cuerpo de la respuesta no coincide");
         });
+
+    it('POST /api/v0/auth/login - debería fallar con contraseña incorrecta (401)', async function() {
+        const badPasswordData = {
+            username: 'secretario1',
+            password: 'password_incorrecto'
+        };
+
+        const res = await request(app)
+            .post('/api/v0/auth/login')
+            .type('form')
+            .send(badPasswordData);
+
+        // 1. Validar Status 200
+        assert.strictEqual(res.status, 401, `Se esperaba 401 pero llegó ${res.status}`);
+        // 2. Validar el cuerpo exacto de la respuesta
+        assert.deepStrictEqual(res.body, {
+            error: "Credenciales inválidas"
+        }, "El cuerpo de la respuesta no coincide");
+
+    });
+
+    it('POST /api/v0/auth/login - debería fallar si faltan campos (400 o 401)', async function() {
+        const res = await request(app)
+            .post('/api/v0/auth/login')
+            .type('form')
+            .send({}); // Enviamos vacío
+
+        // 1. Validar Status 400 (Bad Request) o 401 (Unauthorized)
+        const esErrorValido = res.status === 400 || res.status === 401;
+        assert.ok(esErrorValido, `Status ${res.status} no es de error esperado (400/401)`);
+    });
 });
